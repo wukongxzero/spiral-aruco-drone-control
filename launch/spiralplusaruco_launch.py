b6c032f549af0cf5c65f56ae283df0e1e977150e
wukongxzero <pavankushal15729@gmail.com>
2023-04-22 19:19:52 +0530

    node changes- combined spiral and aruco , the other one is for waypoint

---
+++ launch/spiralplusaruco_launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import IncludeLaunchDescription, Node
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Find the path to the px4_ros_com package
+    PX4_Autopilot_dir = get_package_share_directory('PX4-Autopilot')
+
+    # Include the px4.launch.xml file
+    px4 = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(PX4_Autopilot_dir + '/launch/px4.launch'),
+    )
+
+    # Start the mavsdk_ros node
+    mavsdk_ros = Node(
+        package='mavsdk_ros',
+        executable='mavsdk_ros',
+        name='mavsdk_ros_node',
+        output='screen',
+        parameters=[{'connection_url': 'udp://:14540'}],
+    )
+
+    # Start the go_to_spiralplusaruco_node
+    waypoint_node = Node(
+        package='my_drone_package',
+        executable='spiralplusaruco.py',
+        name='waypoint_node',
+        output='screen',
+    )
+    # Create the launch description object and add the nodes
+    ld = LaunchDescription()
+    ld.add_action(px4)
+    ld.add_action(mavsdk_ros)
+    ld.add_action(waypoint_node)
+
+    return ld
--- setup.py
+++ setup.py
@@ -10,16 +10,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/spiralplusaruco_launch.py']),
+        ('share/' + package_name, ['launch/waypoint_launch.py'])
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='pavan',
-    maintainer_email='pavan@todo.todo',
+    maintainer_email='pavankushal15729@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "spiralplusaruco_node = drone_control.spiralplusaruco_node:main",
+            "waypoint_node = drone_controller.waypoint_node:main"
         ],
     },
 )
